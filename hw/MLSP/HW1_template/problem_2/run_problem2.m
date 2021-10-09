a631b593abe5bec781605d1ed707070b30aacc74
Mo Zhou <cdluminate@gmail.com>
2021-10-09 18:30:05 -0400

    mlsp

--- hw/MLSP/HW1_template/problem_2/run_problem2.m
+++ hw/MLSP/HW1_template/problem_2/run_problem2.m
@@ -1,5 +1,9 @@
 %% Load Notes and Music
 % You may reuse your 'load_data' function from prob 1
+close all;
+clear all;
+[smagNote, smagMusic, sphaseMusic] = load_data();
+fs = 16000;
 
 %% Compute The Transcribe Matrix: non-negative projection with gradient descent
 % Use the 'transcribe_music_gradient_descent' function here
@@ -11,7 +15,52 @@
 % "problem2b_eta_xxx_errorplot.png", where xxx is the eta value.
 % Print the eta vs. E as a bar plot stored in "problem2b_eta_vs_E.png".
 
+num_iter = 250;
+
+[T01, E01, tT, s01] = transcribe_music_gradient_descent( ...
+    smagMusic, smagNote, 0.1, num_iter, 0);
+save('results/problem2b_eta_0.1.dat', 'T01', '-ascii');
+plot(E01);
+title('lr=0.1');
+saveas(gcf, 'results/problem2b_eta_0.1_errorplot.png');
+close gcf;
+
+[T001, E001, tT, s001] = transcribe_music_gradient_descent( ...
+    smagMusic, smagNote, 0.01, num_iter, 0);
+save('results/problem2b_eta_0.01.dat', 'T001', '-ascii');
+plot(E001);
+title('lr=0.01');
+saveas(gcf, 'results/problem2b_eta_0.01_errorplot.png');
+close gcf;
+
+[T0001, E0001, tT, s0001] = transcribe_music_gradient_descent( ...
+    smagMusic, smagNote, 0.001, num_iter, 0);
+save('results/problem2b_eta_0.001.dat', 'T0001', '-ascii');
+plot(E0001);
+title('lr=0.001');
+saveas(gcf, 'results/problem2b_eta_0.001_errorplot.png');
+close gcf;
+
+[T00001, E00001, tT, s00001] = transcribe_music_gradient_descent( ...
+    smagMusic, smagNote, 0.0001, num_iter, 0);
+save('results/problem2b_eta_0.0001.dat', 'T00001', '-ascii');
+plot(E00001);
+title('lr=0.0001');
+saveas(gcf, 'results/problem2b_eta_0.0001_errorplot.png');
+close gcf;
+
+bar([E01(end), E001(end), E0001(end), E00001(end)]);
+xlabel('$0.1^x$');
+ylabel('Last loss');
+saveas(gcf, 'results/problem2b_eta_vs_E.png');
+close gcf;
+
 
 %% Synthesize Music
 % You may reuse the 'synthesize_music' function from prob 1.
 % write the synthesized music as 'polyushka_syn.wav' to the 'results' folder.
+
+recons = synthesize_music(sphaseMusic, s01);
+sound(recons, 16000)
+audiowrite('results/polyushka_syn.wav', recons, 16000);
+disp('this sounds a little bit better than the construction from problem_1');
--- hw/MLSP/HW1_template/problem_2/transcribe_music_gradient_descent.m
+++ hw/MLSP/HW1_template/problem_2/transcribe_music_gradient_descent.m
@@ -11,3 +11,31 @@ function [T, E, transMatT, smagMusicProj] = transcribe_music_gradient_descent(M,
 %   E: num_iter x 1 matrix, error (Frobenius norm) from each iteration
 %   transMatT: 15 x K matrix, threholded version of T (transMat) using threshold
 %   smagMusicProj: 1025 x K matrix, reconstructed version of smagMusic (M) using transMatT
+
+% random initialization
+K = size(M, 2);
+W = ones(15, K);
+DT = prod(size(M));
+loss = norm(M - N * W, 'fro')/DT;
+fprintf('* Initial loss %f\n', loss);
+E(1) = loss;
+
+for i = 1:num_iter
+    % we don't divide the gradient by DT because that will make
+    % the magnitude of gradient tooooooooooooooooooo small.
+    % in that case a much larger learning rate will be needed.
+    gradW = (-2)*(N.' * (M - N * W));%/DT;
+    prevW = W;
+    W = W - lr * gradW;
+    %disp(norm(W - prevW));
+    W = max(W, 0);
+    loss = norm(M - N * W, 'fro')/DT;
+    fprintf('* iter %d, loss=%f, nrm(gradW)=%f\n', i, loss, norm(gradW));
+    E(i+1) = loss;
+end
+T = W;
+
+transMatT = T;
+transMatT(find(transMatT < threshold)) = 0;
+
+smagMusicProj = N * W;
