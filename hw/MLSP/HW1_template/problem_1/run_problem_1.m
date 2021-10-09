c2f02e85a2e9a882950f596fd384988771b9d640
Mo Zhou <cdluminate@gmail.com>
2021-10-09 16:38:06 -0400

    mlsp

--- hw/MLSP/HW1_template/problem_1/run_problem_1.m
+++ hw/MLSP/HW1_template/problem_1/run_problem_1.m
@@ -1,11 +1,41 @@
 %% Load Notes and Music
 % Use the 'load_data' function here
+close all;
+clear all;
+[smagNote, smagMusic, sphaseMusic] = load_data();
+fs = 16000;
+% recon = stft(smagMusic .* sphaseMusic, 2048, 256, 0, hann(2048));
+% sound(recon, 16000)
 
 %% Solution for Problem 1.1 here
 % Place all the 15 scores W_i (for the 15 notes) into a single matrix W. 
 % Place  the score for the i-th note in the i-th row of W.
 % W will be a 15xT matrix, where T is the number of frames in the music.
 % Store W in a text file called "problem2a.dat"
+
+% (a)
+W = zeros(15, 8869);
+for i = 1:15
+    nrmN = smagNote(:, i) / vecnorm(smagNote(:, i), 2);
+    w_i = (smagMusic.' * nrmN).'; % projection
+    W(i, :) = w_i;
+end
+
+% (b)
+M = zeros(size(smagMusic));
+for i = 1:15
+    m = smagNote(:, i) * W(i, :);
+    M = M + m;
+end
+recons = stft(M .* sphaseMusic, 2048, 256, 0, hann(2048));
+sound(recons, fs);
+%clear sound;
+
+% (comment)
+disp('the rythms is basically there. but the fidelity is poor.')
+
+% (save .dat)
+save('problem2a.dat', 'W', '-ascii');
 
 %% Solution to Problem 1.2 here:  Synthesize Music
 % Use the 'synthesize_music' function here.
