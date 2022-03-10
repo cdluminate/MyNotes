160e43c08bca4198335753f06400d0a09ff08dfa
Mo Zhou <cdluminate@gmail.com>
2022-03-10 16:40:13 -0500

    mm

--- T490-Stage.mm
+++ T490-Stage.mm
@@ -63,7 +63,25 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="6" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="15" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Vision-Language Pretraining" POSITION="right" ID="ID_1438251706" CREATED="1646945591468" MODIFIED="1646945596520">
+<edge COLOR="#00ff00"/>
+<node TEXT="CLIP" ID="ID_410734693" CREATED="1646945598394" MODIFIED="1646945599795"/>
+<node TEXT="ALIGN" ID="ID_883337544" CREATED="1646945681124" MODIFIED="1646945684050"/>
+<node TEXT="FILIP" ID="ID_712868502" CREATED="1646945686665" MODIFIED="1646945695226"/>
+<node TEXT="Wukong: 100 Million Large-scale Chinese Cross-modal Pre-training Dataset and A Foundation Framework" ID="ID_579762046" CREATED="1646945599992" MODIFIED="1646945607267">
+<icon BUILTIN="pencil"/>
+</node>
+</node>
+<node TEXT="Classification" POSITION="right" ID="ID_199194756" CREATED="1646942669606" MODIFIED="1646942672604">
+<edge COLOR="#007c00"/>
+<node TEXT="Loss" ID="ID_1501512616" CREATED="1646942673509" MODIFIED="1646942677070">
+<node TEXT="Relational Surrogate Loss Learning" ID="ID_1946575254" CREATED="1646942717232" MODIFIED="1646942719745">
+<icon BUILTIN="unchecked"/>
+<node TEXT="ICLR22" ID="ID_206379015" CREATED="1646942721247" MODIFIED="1646942722686"/>
+</node>
+</node>
+</node>
 <node TEXT="Contrastive Learning" POSITION="right" ID="ID_213839121" CREATED="1646937421326" MODIFIED="1646937424335">
 <edge COLOR="#00ffff"/>
 <node TEXT="BatchFormer: Learning to Explore Sample Relationships for Robust Representation Learning" ID="ID_1047906963" CREATED="1646937425094" MODIFIED="1646937435486">
@@ -76,10 +94,17 @@
 <node TEXT="Do Explanations Explain? Model Knows Best" ID="ID_1458677290" CREATED="1646937968275" MODIFIED="1646937983298">
 <icon BUILTIN="checked"/>
 <node TEXT="CVPR22" ID="ID_164811359" CREATED="1646937985052" MODIFIED="1646937987156"/>
+<node TEXT="one peculiar observation is that these explanations (attributions) point to different features as being important" ID="ID_1757747697" CREATED="1646942060828" MODIFIED="1646942079638"/>
 </node>
 </node>
-<node TEXT="Backdoor Attack" POSITION="right" ID="ID_621969120" CREATED="1646928896139" MODIFIED="1646928902737">
-<edge COLOR="#ff0000"/>
+<node TEXT="Adversarial Attack" POSITION="right" ID="ID_383419135" CREATED="1646945249288" MODIFIED="1646945252281">
+<edge COLOR="#007c7c"/>
+<node TEXT="Universal Attack" ID="ID_1756746093" CREATED="1646945253221" MODIFIED="1646945259324">
+<node TEXT="Universal Adversarial Examples in Remote Sensing: Methodology and Benchmark" ID="ID_1566703454" CREATED="1646945260207" MODIFIED="1646945261393"/>
+</node>
+</node>
+<node TEXT="Backdoor Attack" POSITION="right" ID="ID_621969120" CREATED="1646928896139" MODIFIED="1646945565648">
+<edge COLOR="#0000ff"/>
 <node TEXT="Automobile" ID="ID_1807292891" CREATED="1646928903901" MODIFIED="1646928910994">
 <node TEXT="Clean-Annotation Backdoor Attack against Lane Detection Systems in the Wild" ID="ID_444510611" CREATED="1646928911957" MODIFIED="1646928920668">
 <icon BUILTIN="pencil"/>
@@ -88,8 +113,30 @@
 </node>
 </node>
 </node>
+<node TEXT="Backdoor Defense" POSITION="right" ID="ID_1012849347" CREATED="1646945548416" MODIFIED="1646945551473">
+<edge COLOR="#ff0000"/>
+<node TEXT="A Survey of Neural Trojan Attacks and Defenses in Deep Learning" ID="ID_941695665" CREATED="1646945552550" MODIFIED="1646945574224">
+<icon BUILTIN="pencil"/>
+</node>
+<node TEXT="Adversarial Fine-tuning for Backdoor Defense: Connect Adversarial Examples to Triggered Samples" ID="ID_580929337" CREATED="1646947640727" MODIFIED="1646947646923">
+<icon BUILTIN="pencil"/>
+</node>
+</node>
 <node TEXT="Attack Detection" POSITION="right" ID="ID_1704034770" CREATED="1646929038562" MODIFIED="1646929042691">
 <edge COLOR="#0000ff"/>
+<node TEXT="Survey" ID="ID_532349138" CREATED="1646943062780" MODIFIED="1646943065197">
+<node TEXT="Adversarial Example Detection for DNN Models: A Review and Experimental Comparison" ID="ID_1054333884" CREATED="1646943075067" MODIFIED="1646943080350">
+<icon BUILTIN="bookmark"/>
+<icon BUILTIN="pencil"/>
+</node>
+</node>
+<node TEXT="Detect" ID="ID_1538641735" CREATED="1646943241545" MODIFIED="1646943242980">
+<node TEXT="Detecting Adversarial Samples from Artifacts" ID="ID_378405709" CREATED="1646943244582" MODIFIED="1646943253077">
+<icon BUILTIN="bookmark"/>
+<icon BUILTIN="unchecked"/>
+<node TEXT="ICML17" ID="ID_539936003" CREATED="1646943246293" MODIFIED="1646943248413"/>
+</node>
+</node>
 <node TEXT="multitask" ID="ID_141398745" CREATED="1646929096139" MODIFIED="1646932700896">
 <node TEXT="Detecting Adversarial Perturbations in Multi-Task Perception" ID="ID_274457308" CREATED="1646929102998" MODIFIED="1646932772460">
 <icon BUILTIN="pencil"/>
@@ -109,6 +156,15 @@
 </node>
 </node>
 </node>
+<node TEXT="Deepfake detection" POSITION="right" ID="ID_1060157239" CREATED="1646942110396" MODIFIED="1646942115269">
+<edge COLOR="#00007c"/>
+<node TEXT="multi-modality" ID="ID_692707786" CREATED="1646942308628" MODIFIED="1646942311937">
+<node TEXT="Voice-Face Homogeneity Tells Deepfake" ID="ID_153357596" CREATED="1646942317853" MODIFIED="1646942325559">
+<icon BUILTIN="unchecked"/>
+<node TEXT="CVPR22" ID="ID_725170575" CREATED="1646942319120" MODIFIED="1646942321768"/>
+</node>
+</node>
+</node>
 <node TEXT="Adversarial Defense" POSITION="right" ID="ID_1792455725" CREATED="1646933220976" MODIFIED="1646933224968">
 <edge COLOR="#00ff00"/>
 <node TEXT="Reverse Cross Entropy" ID="ID_1881427141" CREATED="1646933225738" MODIFIED="1646933231428">
@@ -124,6 +180,27 @@
 </node>
 </node>
 </node>
+<node TEXT="Regularization" ID="ID_1589633427" CREATED="1646943291436" MODIFIED="1646943297881">
+<node TEXT="Learning Smooth Neural Functions via Lipschitz Regularization" ID="ID_70062564" CREATED="1646943406180" MODIFIED="1646943409938">
+<icon BUILTIN="unchecked"/>
+<node TEXT="SIGGRAPH" ID="ID_913247296" CREATED="1646943412096" MODIFIED="1646943415898"/>
+<node TEXT="not adv attack, but there is smooth constraint" ID="ID_1894803830" CREATED="1646943416298" MODIFIED="1646943426597"/>
+</node>
+</node>
+</node>
+<node TEXT="Intersection Defense" POSITION="right" ID="ID_1176631817" CREATED="1646947990676" MODIFIED="1646947995010">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Openset Recognition" ID="ID_417719416" CREATED="1646947995726" MODIFIED="1646948002223">
+<node TEXT="Open-set Adversarial Defense with Clean-Adversarial Mutual Learning" ID="ID_109010590" CREATED="1646948002881" MODIFIED="1646948362746">
+<icon BUILTIN="unchecked"/>
+<node TEXT="IJCV" ID="ID_1364434112" CREATED="1646948008562" MODIFIED="1646948010165"/>
+</node>
+</node>
+<node TEXT="Deepfake Detection" ID="ID_1986728438" CREATED="1646948353156" MODIFIED="1646948357613">
+<node TEXT="Towards Adversarially Robust Deepfake Detection: An Ensemble Approach" ID="ID_1602704665" CREATED="1646948358406" MODIFIED="1646948364713">
+<icon BUILTIN="pencil"/>
+</node>
+</node>
 </node>
 <node TEXT="Privacy" POSITION="right" ID="ID_545030493" CREATED="1646937276323" MODIFIED="1646937278744">
 <edge COLOR="#ff00ff"/>
@@ -132,6 +209,17 @@
 <icon BUILTIN="checked"/>
 <font ITALIC="false"/>
 </node>
+<node TEXT="Do Gradient Inversion Attacks Make Federated Learning Unsafe?" ID="ID_539563592" CREATED="1646948139524" MODIFIED="1646948148268">
+<icon BUILTIN="unchecked"/>
+</node>
+<node TEXT="Assessing Privacy Risks from Feature Vector Reconstruction Attacks" ID="ID_456202389" CREATED="1646948272071" MODIFIED="1646948274903"/>
+</node>
+</node>
+<node TEXT="Cross-Modal Retrieval" POSITION="right" ID="ID_544557779" CREATED="1646944256452" MODIFIED="1646944262160">
+<edge COLOR="#7c007c"/>
+<node TEXT="Cross-Modal Common Representation Learning with Triplet Loss Functions" ID="ID_1827284412" CREATED="1646944290551" MODIFIED="1646944296357">
+<icon BUILTIN="pencil"/>
+<node TEXT="fair" ID="ID_1712372002" CREATED="1646944300395" MODIFIED="1646944304415"/>
 </node>
 </node>
 </node>
