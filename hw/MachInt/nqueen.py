ad9f23981f3fd740e6936e361f26fd1c3fb1d9b1
Mo Zhou <cdluminate@gmail.com>
2022-04-05 16:31:01 -0400

    py

--- hw/MachInt/nqueen.py
+++ hw/MachInt/nqueen.py
@@ -1,40 +1,62 @@
-import numpy as np
+#!/usr/bin/python3
 import rich
+from collections import Counter
 from rich.progress import track
 c = rich.get_console()
 
-def validate(rep: np.ndarray) -> bool:
+def isvalid(v: list, n: int = None, debug: bool = False) -> bool:
     '''
-    Validate the given solution. Both vector and square
-    matrix representations are acceptable.
+    Tells whether the given vector `v` is a valid solution for n-queen.
+    The vector is a row-based representation of the checkboard.
+    v[0] == 0 means the queen for the first row is placed at the first column.
     '''
-    if len(rep.shape) == 1:
-        rep2 = np.zeros((len(rep), len(rep)))
-        rep2[np.arange(len(rep)), rep] = 1
-    elif len(rep.shape) == 2:
-        rep2 = rep
-    else:
-        raise ValueError('invalid shape for solution')
-    N = rep2.shape[0]
-    # check1: row conflict
-    if not all(rep2.sum(axis=1) <= 1):
+    # dealing with convenience call
+    if v and (n is None):
+        n = len(v)
+    # helper function for checking diagonal attack
+    def _diag_attack(_v: list, debug: bool = False):
+        c = Counter(i - _v[i] for i in range(len(_v)))
+        return any(count > 1 for count in c.values())
+    # check1: completeness
+    if len(v) < n:
+        if debug:
+            print('Reason: incomplete', len(v), n)
         return False
-    # check2: column conflict
-    if not all(rep2.sum(axis=0) <= 1):
+    # check2: column attack
+    # note, row representation is free from row attack
+    if not all(x <= 1 for x in Counter(v).values()):
+        if debug:
+            print('Reason: column attack', Counter(v))
         return False
-    # check3: diagonals conflict
-    if not all(rep2.diagonal(i).sum() <=1
-            for i in range(-(N-1),N)):
+    # check3: diagonal attack
+    if _diag_attack(v, debug) or _diag_attack(list(reversed(v)), debug):
+        if debug:
+            print('Reason: diagonal attack')
         return False
-    # check4: opposite diagonals conflict
-    if not all(rep2[::-1,:].diagonal(i).sum() <=1 for i in range(-(N-1),N)):
-        return False
+    # passed all checks
     return True
 
 
+def cbdump(v: list) -> str:
+    '''
+    Construct a textural representation of the checkboard.
+    '''
+    n = len(v)
+    rows = [['Q ' if v[i]==j else '. ' for j in range(n)] for i in range(n)]
+    rows = '\n'.join([''.join(row) for row in rows])
+    return rows
+
+
 def test_validate():
-    sol = np.array([5,3,1,7,2,8,6,4]) - 1
-    assert(validate(sol))
+    sol = [x-1 for x in [5,3,1,7,2,8,6,4]]
+    c.print(cbdump(sol))
+    assert(isvalid(sol, debug=True))
+    sol = [x-1 for x in [1,1,1,1,1,1,1,1]]
+    c.print(cbdump(sol))
+    assert(not isvalid(sol, debug=True))
+    sol = [x-1 for x in [1,2,3,4,5,6,7,8]]
+    c.print(cbdump(sol))
+    assert(not isvalid(sol, debug=True))
 
 if __name__ == '__main__':
     test_validate()
