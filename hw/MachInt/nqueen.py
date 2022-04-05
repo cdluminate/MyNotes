6ccbac26eeda4499adb3891f7d5d9226e4c23ffd
Mo Zhou <cdluminate@gmail.com>
2022-04-05 16:51:21 -0400

    py

--- hw/MachInt/nqueen.py
+++ hw/MachInt/nqueen.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python3
+#!/usr/bin/pypy3
+import time
 import rich
 from collections import Counter
 from rich.progress import track
@@ -37,16 +38,6 @@ def isvalid(v: list, n: int = None, debug: bool = False) -> bool:
     return True
 
 
-def cbdump(v: list) -> str:
-    '''
-    Construct a textural representation of the checkboard.
-    '''
-    n = len(v)
-    rows = [['Q ' if v[i]==j else '. ' for j in range(n)] for i in range(n)]
-    rows = '\n'.join([''.join(row) for row in rows])
-    return rows
-
-
 def test_validate():
     sol = [x-1 for x in [5,3,1,7,2,8,6,4]]
     c.print(cbdump(sol))
@@ -58,5 +49,64 @@ def test_validate():
     c.print(cbdump(sol))
     assert(not isvalid(sol, debug=True))
 
+
+def cbdump(v: list) -> str:
+    '''
+    Construct a textural representation of the checkboard.
+    '''
+    n = len(v)
+    rows = [['Q ' if v[i]==j else '. ' for j in range(n)] for i in range(n)]
+    rows = '\n'.join([''.join(row) for row in rows])
+    return rows
+
+
+def solve_nqueen_dfs(n: int):
+    '''
+    Naive n-queen solver in dfs, brute-force.
+    '''
+    return _solve_nqueen_dfs([], n)
+
+
+def _solve_nqueen_dfs(v: list, n: int):
+    '''
+    recursive worker function for solve_nqueen_dfs.
+    validate solution when reached the leaf node
+    '''
+    cursor = len(v)
+    # recursion boundary
+    if cursor == n:
+        if isvalid(v):
+            return v
+        else:
+            return False
+    else:
+        for i in range(n):
+            v.append(i)
+            ret = _solve_nqueen_dfs(v, n)
+            if ret:
+                return ret
+            else:
+                v.pop()
+
+
+def benchmark_nqueen(n: int):
+    '''
+    benchmark differnt solvers and make plot
+    '''
+    solvers = [solve_nqueen_dfs]
+    elapsed = []
+    for solver in solvers:
+        tm_start = time.time()
+        sol = solver(n)
+        tm_end = time.time()
+        elapsed.append(tm_end - tm_start)
+    print('Elapsed time:')
+    for i in range(len(solvers)):
+        print(f'[{i}]', solvers[i].__name__, elapsed[i])
+
+
 if __name__ == '__main__':
-    test_validate()
+    #test_validate()
+    #v = solve_nqueen_dfs(8)
+    #c.print(cbdump(v))
+    benchmark_nqueen(8)
