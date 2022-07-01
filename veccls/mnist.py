7bb869154340a9eed1d835556b32b772208af5c6
Mo Zhou <cdluminate@gmail.com>
2022-07-01 16:28:24 -0700

    mnist collect

--- veccls/mnist.py
+++ veccls/mnist.py
@@ -5,7 +5,10 @@ import argparse
 import torchvision as V
 import matplotlib.pyplot as plt
 import ujson as json
+import glob
+import re
 import rich
+from rich.progress import track
 console = rich.get_console()
 
 def mnist_burst(dest: str, split: str):
@@ -23,16 +26,34 @@ def mnist_burst(dest: str, split: str):
     data = V.datasets.MNIST(root=os.path.expanduser('~/.torch/MNIST'),
                             train=True if split=='train' else False)
     for i, (x, y) in enumerate(data):
-        fpath = os.path.join(dest, '%05d.png'%i)
+        fpath = os.path.join(dest, '%05d-%1d.png'%(i, y))
         xinv = PIL.ImageOps.invert(x)
         xinv.save(fpath)
         console.print(fpath)
         print(x, y)
     print(len(data))
 
+
+def mnist_collect(src: str):
+    '''
+    collect processed data from
+    '''
+    dataset = []
+    files = glob.glob(os.path.join(src, '*.json'))
+    for file in track(files):
+        with open(file, 'rt') as f:
+            j = json.load(f)
+        label = re.match(r'\d+-(\d).json', file).groups()[0]
+        j[0]['label'] = int(label)
+        dataset.append(j)
+    with open(src+'.json', 'wt') as f:
+        json.dump(dataset, f)
+    console.print('>_< done')
+
+
 if __name__ == '__main__':
     ag = argparse.ArgumentParser('python3 -m veccls.mnist')
-    ag.add_argument('action', choices=('burst',))
+    ag.add_argument('action', choices=('burst', 'collect'))
     ag.add_argument('-d', '--destination', default='.', help='dest directory')
     ag.add_argument('-s', '--split', default='train', choices=('train', 'test'))
     ag = ag.parse_args()
@@ -40,3 +61,5 @@ if __name__ == '__main__':
 
     if ag.action == 'burst':
         mnist_burst(ag.destination, ag.split)
+    elif ag.action == 'collect':
+        mnist_collect(ag.destination)
