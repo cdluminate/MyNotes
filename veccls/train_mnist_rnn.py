f31efc1b54bc39faa5ef46427ff9f5c6f26dd1c6
Mo Zhou <cdluminate@gmail.com>
2022-07-01 23:16:22 -0700

    train_mnist_rnn

--- veccls/train_mnist_rnn.py
+++ veccls/train_mnist_rnn.py
@@ -5,6 +5,7 @@ import torch.nn.functional as F
 from . import mnist_dataset
 from torch.nn.utils.rnn import pack_padded_sequence, unpack_sequence
 import rich
+from rich.progress import track
 console = rich.get_console()
 
 class MnistRNN(th.nn.Module):
@@ -54,7 +55,7 @@ def train_one_epoch(model, optim, loader,
         *,
         epoch: int = -1,
         device: str = 'cpu',
-        report_every: int = 10):
+        report_every: int = 50):
     '''
     train for one epoch, literally
     '''
@@ -69,7 +70,7 @@ def train_one_epoch(model, optim, loader,
         loss.backward()
         optim.step()
 
-        if (i+1)%report_every == 0:
+        if (i)%report_every == 0:
             pred = logits.max(dim=1)[1]
             acc = (pred == y).cpu().float().mean().item() * 100
             console.print(f'Eph[{epoch}] ({i+1}/{len(loader)})',
@@ -77,9 +78,38 @@ def train_one_epoch(model, optim, loader,
                     f'accuracy={acc:.2f} (/100)',
                     )
 
+@th.no_grad()
+def evaluate(model, loader,
+        *,
+        epoch: int = -1,
+        device: str = 'cpu',
+        ):
+    '''
+    evaluate, literally
+    '''
+    losses = []
+    preds = []
+    ys = []
+    for (x, y, z) in track(loader, description=f'Evaluation ...'):
+        pack = pack_padded_sequence(x, z)
+        pack = pack.to(device)
+        y = y.to(device)
+        logits = model(pack)
+        loss = F.cross_entropy(logits, y, reduction='none')
 
-def evaluate(model, loader):
-    pass
+        losses.append(loss)
+        ys.append(y)
+        preds.append(logits.max(dim=1)[1])
+    losses = th.hstack(losses)
+    preds = th.hstack(preds)
+    ys = th.hstack(ys)
+    #console.print(losses.shape, preds.shape, ys.shape)
+    mean_loss = losses.mean()
+    acc = (preds == ys).cpu().float().mean().item() * 100
+    console.print(f'Eph[{epoch}] Evaluation:',
+            f'loss={mean_loss:.2f}',
+            f'acc={acc:.2f} (/100)')
+
 
 if __name__ == '__main__':
     ag = argparse.ArgumentParser()
@@ -105,11 +135,16 @@ if __name__ == '__main__':
     console.print(f'-- Number of parameters:', model.num_params)
     optim = th.optim.Adam(model.parameters(), lr=ag.lr)
 
-    loader = mnist_dataset.get_mnist_loader(split='train',
+    loadertrn = mnist_dataset.get_mnist_loader(split='train',
             batch_size=128, longest=True)
+    loadertst = mnist_dataset.get_mnist_loader(split='test',
+            batch_size=100, longest=True)
 
     for epoch in range(ag.epochs):
-        console.print(f'>_< training epoch {epoch}')
+        console.print(f'>_< training epoch {epoch} ...')
 
-        train_one_epoch(model, optim, loader,
+        train_one_epoch(model, optim, loadertrn,
+                epoch=epoch, device=ag.device)
+
+        evaluate(model, loadertst,
                 epoch=epoch, device=ag.device)
