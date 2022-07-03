820076135018aedd83ca19c1f61e7a69ff722e1d
Mo Zhou <cdluminate@gmail.com>
2022-07-02 17:06:18 -0700

    train_fashion: support pst as well

--- veccls/train_fashion.py
+++ veccls/train_fashion.py
@@ -6,6 +6,7 @@ from . import fashion_dataset
 import rich
 from rich.progress import track
 from . import recurrent
+from . import transformer
 from . import engine
 console = rich.get_console()
 
@@ -13,14 +14,15 @@ console = rich.get_console()
 if __name__ == '__main__':
     ag = argparse.ArgumentParser('''Train an Fashion model, vector graphics!
     ''')
-    # RNN settings
+    # recurrent neural network and transformer settings
     ag.add_argument('--model_type', type=str, default='gru',
             choices=('rnn', 'gru', 'lstm',
-                'hrnn', 'hgru', 'hlstm',
-                ))
+                'hrnn', 'hgru', 'hlstm', 'pst', 'hpst'))
     ag.add_argument('--input_size', type=int, default=2)
     ag.add_argument('--hidden_size', type=int, default=64)
     ag.add_argument('--num_layers', type=int, default=3)
+    ag.add_argument('--nhead', type=int, default=2)
+    ag.add_argument('--dropout', type=float, default=0.1)
     # optimizer and training setting
     ag.add_argument('--lr', type=float, default=1e-3)
     ag.add_argument('--weight_decay', type=float, default=1e-7)
@@ -48,9 +50,25 @@ if __name__ == '__main__':
             'hrnn': recurrent.HierarchicalRNN,
             'hgru': recurrent.HierarchicalRNN,
             'hlstm': recurrent.HierarchicalRNN,
+            # only use the longest path
+            'pst': transformer.LongestPathTransformer,
+            # use all paths
+            'hpst': transformer.HierarchicalTransformer,
             }
-    model = modelmapping[ag.model_type](ag.model_type,
-            ag.input_size, ag.hidden_size, ag.num_layers).to(ag.device)
+    if ag.model_type in ('rnn', 'gru', 'lstm', 'hrnn', 'hgru', 'hlstm'):
+        # is recurrent model
+        model = modelmapping[ag.model_type](ag.model_type,
+                ag.input_size, ag.hidden_size, ag.num_layers).to(ag.device)
+    else:
+        # is transformer
+        model = modelmapping[ag.model_type](
+                model_type=ag.model_type,
+                input_size=ag.input_size,
+                d_model=ag.hidden_size,
+                nhead=ag.nhead,
+                d_mlp=ag.hidden_size,
+                nlayers=ag.num_layers,
+                dropout=ag.dropout).to(ag.device)
     console.print(model)
     console.print(f'-- Number of parameters:', model.num_params)
     optim = th.optim.Adam(model.parameters(),
