98ebe1c2b373e37b669a18630da960d4ad4cb263
Mo Zhou <cdluminate@gmail.com>
2022-07-03 12:58:52 -0700

    only rank 0 prints

--- veccls/train_mnist.py
+++ veccls/train_mnist.py
@@ -82,8 +82,9 @@ def train_mnist():
                 d_mlp=ag.hidden_size,
                 nlayers=ag.num_layers,
                 dropout=ag.dropout).to(ag.device)
-    console.print(model)
-    console.print(f'-- Number of parameters:', model.num_params)
+    if ag.local_rank is None or th.distributed.get_rank() == 0:
+        console.print(model)
+        console.print(f'-- Number of parameters:', model.num_params)
     if ag.local_rank is not None:
         model = th.nn.parallel.DistributedDataParallel(model,
                 device_ids=[ag.local_rank], output_device=ag.local_rank)
@@ -91,8 +92,9 @@ def train_mnist():
         lr=ag.lr, weight_decay=ag.weight_decay)
     scheduler = th.optim.lr_scheduler.MultiStepLR(optim,
             milestones=[ag.lr_drop], gamma=0.1)
-    console.print(f'-- Optimizer:', optim)
-    console.print(f'-- Scheduler:', scheduler)
+    if ag.local_rank is None or th.distributed.get_rank() == 0:
+        console.print(f'-- Optimizer:', optim)
+        console.print(f'-- Scheduler:', scheduler)
 
     loadertrn = mnist_dataset.get_mnist_loader(split='train',
             batch_size=128,
