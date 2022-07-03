6b33355c3dd658c78d4069ac2d22c80dfc6ee10a
Mo Zhou <cdluminate@gmail.com>
2022-07-02 21:53:43 -0700

    small trick

--- veccls/transformer.py
+++ veccls/transformer.py
@@ -70,11 +70,6 @@ class LongestPathTransformer(th.nn.Module):
         self.num_classes = num_classes
         self.num_params = sum(param.numel() for param in self.parameters()
                 if param.requires_grad)
-    def forward_(self, src: th.Tensor, src_mask: th.Tensor):
-        src = self.encoder(src) * math.sqrt(self.d_model)
-        src = self.posenc(src)
-        output = self.transenc(src, src_mask)
-        return output
     def gen_mask(self, lens: th.Tensor):
         mask = [[1]*i + [0]*(lens.max().item()-i) for i in lens]
         mask = th.tensor(mask)
@@ -90,7 +85,7 @@ class LongestPathTransformer(th.nn.Module):
         B = int(x.shape[1])
         mask = self.gen_mask(z.lens)
         mask = mask.to(device)
-        xe = self.encoder(x.to(device))
+        xe = self.encoder(x.to(device)) * math.sqrt(self.d_model)  # helpful
         xepe = self.posenc(xe)
         #print('debug', xepe.shape, mask.shape)
         memory = self.transenc(xepe, src_key_padding_mask=mask)
@@ -123,7 +118,73 @@ class HierarchicalPathTransformer(th.nn.Module):
             dropout: float = 0.01,
             num_classes: int = 10,
             layernorm: bool = False):
+        super(HierarchicalPathTransformer, self).__init__()
         self.model_type = model_type
+        # path: positional encoding
+        self.posenc = PositionalEncoding(d_model=d_model, dropout=dropout)
+        # path: transformer encoder
+        enclayer = th.nn.TransformerEncoderLayer(d_model, nhead, d_mlp, dropout)
+        encoder_norm = th.nn.LayerNorm(d_model) if layernorm else None
+        self.pathenc = th.nn.TransformerEncoder(enclayer, nlayers, encoder_norm)
+        # path: translate + color encoder
+        self.pathtc = th.nn.Sequential(
+                th.nn.Linear(5, d_mlp), th.nn.ReLU(),
+                th.nn.Linear(d_mlp, d_mlp), th.nn.ReLU(),
+                th.nn.Linear(d_mlp, d_model), th.nn.ReLU(),
+                )
+        # path: transformer decoder
+        declayer = th.nn.TransformerDecoderLayer(d_model, nhead, d_mlp, dropout)
+        decoder_norm = th.nn.LayerNorm(d_model) if layernorm else None
+        self.pathdec = th.nn.TransformerDecoder(declayer, nlayers, decoder_norm)
+        # path: encode 2-d trajectory
+        self.xyenc = th.nn.Linear(input_size, d_model) # XXX: no MLP here
+        # svg: transformer encoder
+        senclayer = th.nn.TransformerEncoderLayer(d_model, nhead, d_mlp, dropout)
+        self.svgenc = th.nn.TransformerEncoder(senclayer, nlayer, encoder_norm)
+        # svg: transformer decoder
+        sdeclayer = th.nn.TransformerDecoderLayer(d_model, nhead, d_mlp, dropout)
+        self.svgdec = th.nn.TransformerDecoder(sdeclayer, nlayer, decoder_norm)
+        # svg: classification head
+        self.fc = th.nn.Linear(d_model, num_classes)
+        # other parameters
+        self.d_model = d_model
+        self.num_classes = num_classes
+        self.num_params = sum(param.numel() for param in self.parameters()
+                if param.requires_grad)
+    def gen_mask(self, lens: th.Tensor):
+        mask = [[1]*i + [0]*(lens.max().item()-i) for i in lens]
+        mask = th.tensor(mask)
+        return mask
+    def forward(self, x, y, z, *, device: str = 'cpu'):
+        '''
+        x is padded sequence
+        y is labels
+        z is EasyDict, keys:
+            tc: translate and color info
+            lens: sequence lengths
+        '''
+        B = int(x.shape[1])
+        mask = self.gen_mask(z.lens)
+        mask = mask.to(device)
+        xe = self.encoder(x.to(device)) * math.sqrt(self.d_model)
+        xepe = self.posenc(xe)
+        #print('debug', xepe.shape, mask.shape)
+        memory = self.transenc(xepe, src_key_padding_mask=mask)
+        #print('debug', memory.shape)
+        # [begin: no decoder case]
+        # use hidden state corrosponding the first token. following BERT.
+        #h = memory[0, ...]
+        #logits = self.fc(h)
+        #print('logits', logits.shape)
+        # [end: no decoder case]
+        # [begin: has decoder]
+        tgt = self.pathtc(z.tc.to(device)).unsqueeze(0)
+        hs = self.transdec(tgt, memory, memory_key_padding_mask=mask)
+        hs = hs.squeeze(0)
+        #print('debug:', hs.shape)
+        logits = self.fc(hs)
+        # [end: has decoder]
+        return logits
 
 
 if __name__ == '__main__':
