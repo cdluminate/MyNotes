e15dde40986e56aae0266032fa355ade1bf3bcbf
Mo Zhou <cdluminate@gmail.com>
2022-12-06 18:41:34 -0500

    algo:lc

--- algo/lc.118.py
+++ algo/lc.118.py
@@ -17,3 +17,22 @@ class Solution(object):
                 for j in range(1,len(iline)-1):
                     iline[j] = tria[i-1][j-1] + tria[i-1][j]
         return tria
+
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows == 0:
+            return []
+        # numRows > 0
+        tri = []
+        for i in range(1, numRows+1):
+            row = []
+            for j in range(i):
+                if j == 0 or j == i-1:
+                    row.append(1)
+                else:
+                    a = tri[i-2][j-1]
+                    b = tri[i-2][j]
+                    row.append(a + b)
+            tri.append(row)
+        return tri
