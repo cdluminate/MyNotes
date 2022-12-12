fe213f14d0755f8b0d93caefeccb4c2365636473
Mo Zhou <cdluminate@gmail.com>
2022-12-12 14:12:23 -0500

    puftm: add engine for common train/test routine

--- puftm/__init__.py
+++ puftm/__init__.py
@@ -1,1 +1,4 @@
+from . import config
+from . import datasets
 from . import lenet
+from . import engine
--- puftm/config.py
+++ puftm/config.py
@@ -12,3 +12,8 @@ class datasets:
         root : str = os.path.expanduser('~/.torch/MNIST')
         batch_size : int = 100
         num_workers : int = 0
+
+
+@dataclass
+class engine:
+    report_every: int = 10
