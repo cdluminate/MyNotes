50153939db681e45012abd1c793ce5136f8679c9
Mo Zhou <cdluminate@gmail.com>
2023-03-21 14:26:24 -0400

    rs: rob

--- rs/rob/cifar10-r50-adv.sh
+++ rs/rob/cifar10-r50-adv.sh
@@ -2,26 +2,31 @@
 set -e
 set -x
 export NCCL_P2P_DISABLE=1
-export CUDA_VISIBLE_DEVICES=7
+export CUDA_VISIBLE_DEVICES=6
 LOGDIR=${0%.sh}
 if ! test -d ${LOGDIR}; then
 	mkdir -p ${LOGDIR}
 	python -m robustness.main --dataset cifar --data ~/.torch/ \
-	       --adv-train 0 \
-	       --arch resnet50 \
 	       --out-dir ${LOGDIR} \
+	       --arch resnet50 \
+	       --adv-train 1 \
+	       --constraint inf \
+	       --eps $(python3 -c "print(8./255.)") \
+	       --attack-lr 2 \
+	       --attack-steps 20 \
 	       | tee ${LOGDIR}/train.log
 fi
 LOGDIR_E20=${LOGDIR}/attack-eval-20/
 if ! test -d ${LOGDIR_E20}; then
 	mkdir -p ${LOGDIR_E20}
 	python -m robustness.main --dataset cifar --data ~/.torch/ \
-		--eval-only 1 \
+		--out-dir ${LOGDIR_E20} \
 		--arch resnet50 \
+		--eval-only 1 \
 		--adv-eval 1 \
 		--constraint inf \
-		--out-dir ${LOGDIR_E20} \
-		--eps 0.03137254901960784 --attack-lr 2 \
+		--eps $(python3 -c "print(8./255.)") \
+		--attack-lr 2 \
 		--attack-steps 20 \
 		--resume $(find ${LOGDIR} -type f -name '*.pt.best' | head -n1) \
 		| tee ${LOGDIR_E20}/eval.log
