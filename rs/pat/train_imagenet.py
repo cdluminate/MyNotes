46a31fe9ec8a0ef03b1544f0cb0b70a1b683099a
Mo Zhou <cdluminate@gmail.com>
2023-09-28 19:56:32 -0400

    pat: u

--- rs/pat/train_imagenet.py
+++ rs/pat/train_imagenet.py
@@ -525,7 +525,7 @@ class ProgressMeter(object):
         print('\t'.join(entries))
         
     def display_summary(self):
-        entries = [" *"]
+        entries = ["Eval *"]
         entries += [meter.summary() for meter in self.meters]
         print(' '.join(entries))
 
