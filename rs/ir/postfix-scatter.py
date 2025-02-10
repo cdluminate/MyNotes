919542ab8aace87a22bd447bef962cd70b7a9ac7
Mo Zhou <cdluminate@gmail.com>
2025-02-10 10:15:21 -0500

    rs:ir

--- rs/ir/postfix-scatter.py
+++ rs/ir/postfix-scatter.py
@@ -15,23 +15,34 @@ def read_float(path: str):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Make scatter plots based on specified file postfixes.')
-    parser.add_argument('--read', '-r', type=str, required=True, help='Path to the file to read.')
+    parser.add_argument('--read', '-r', type=str, required=True, action='append', help='Path to the file to read.')
     parser.add_argument('--glob', '-g', type=str, default='*.png', help='Glob pattern to match files.')
     parser.add_argument('--x', '-x', type=str, required=True, help='Postfix for x axis.')
     parser.add_argument('--y', '-y', type=str, required=True, help='Postfix for y axis.')
     parser.add_argument('--title', type=str, default='', help='Title of the plot.')
 
     args = parser.parse_args()
+    print('reading directories:', args.read)
 
     # read data
-    images = glob.glob(os.path.join(args.read, args.glob))
+    images = glob.glob(os.path.join(args.read[0], args.glob))
+    basenames = [os.path.basename(i) for i in images]
     print('#images:', len(images))
-    xdata = [read_float(i + '.' + args.x) for i in images]
-    ydata = [read_float(i + '.' + args.y) for i in images]
 
     # make scatter plot
     plt.figure()
+
+    xdata = [read_float(i + '.' + args.x) for i in images]
+    ydata = [read_float(i + '.' + args.y) for i in images]
     plt.scatter(xdata, ydata)
+
+    for readdir in args.read[1:]:
+        images = [os.path.join(readdir, i) for i in basenames]
+        xdata = [read_float(i + '.' + args.x) for i in images]
+        ydata = [read_float(i + '.' + args.y) for i in images]
+        plt.scatter(xdata, ydata)
+
+    plt.legend(args.read)
     plt.xlabel(args.x)
     plt.ylabel(args.y)
     plt.title(args.title)
