7a191d15fa5daffc99960b137412964172d751b3
Mo Zhou <cdluminate@gmail.com>
2025-08-21 11:02:09 -0400

    lc

--- algo/lc.206.py
+++ algo/lc.206.py
@@ -5,6 +5,20 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, current = None, head
+        while current is not None:
+            tmp = prev
+            prev, current = current, current.next
+            prev.next = tmp
+        return prev
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if head is None:
             return None
         elif head.next is None:
