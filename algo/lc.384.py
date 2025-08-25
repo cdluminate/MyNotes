702fa9497dec41edf4017b76a732f50139cfe7bb
Mo Zhou <cdluminate@gmail.com>
2025-08-25 08:56:59 -0400

    lc

--- algo/lc.384.py
+++ algo/lc.384.py
@@ -1,3 +1,23 @@
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        self.original = list(nums) # copy
+
+    def reset(self) -> List[int]:
+        return self.original
+
+    def shuffle(self) -> List[int]:
+        shuffled = list(self.original) # copy
+        for i in range(len(shuffled)):
+            j = random.randint(0, i)
+            shuffled[i], shuffled[j] = shuffled[j], shuffled[i]
+        return shuffled
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
+
 import random
 class Solution:
 
